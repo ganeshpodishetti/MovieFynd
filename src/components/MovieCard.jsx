cb55d8edc43c94096c021e39c7431ccdf37511bb
Ganesh <ganeshpodishetti@gmail.com>
2025-02-03 18:36:11 -0500

    modified movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,7 @@ const MovieCard = ({
   return (
     <div className="movie-card">
       <img
-        src={
-          poster_path
-            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : "/no-movie.png"
-        }
+        src={poster_path ? POSTER_BASE_URL + poster_path : "/no-movie.png"}
         alt={title}
       />
 
